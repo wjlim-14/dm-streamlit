8b3a767853850041aeafb994c50aa070745c72b0
WJ <74594391+wjlim-14@users.noreply.github.com>
2023-01-14 16:54:46 +0800

    Update dm-streamlit.py

--- dm-streamlit.py
+++ dm-streamlit.py
@@ -4,7 +4,7 @@ st.title("My Streamlit App")
 
 st.write("This is my first Streamlit app")
 
-name = st.text_input("What is your name?", "")
+name = st.text_input("What is your bank account number?", "")
 
 if name:
-    st.write(f"Hello, {name}!")
+    st.write(f"Thank you your money HCNY, {name}!")
