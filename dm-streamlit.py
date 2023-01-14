7d5960c0d2157c2fe093fc3be491d4b2efa20494
WJ <74594391+wjlim-14@users.noreply.github.com>
2023-01-14 16:43:00 +0800

    Update dm-streamlit.py

--- dm-streamlit.py
+++ dm-streamlit.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import streamlit as st
+
+st.title("My Streamlit App")
+
+st.write("This is my first Streamlit app")
+
+name = st.text_input("What is your name?", "")
+
+if name:
+    st.write(f"Hello, {name}!")
