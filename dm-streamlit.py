0086bf518f71f7ff373f7e01df0bcf9e64cca6f9
SeeJie <59280059+seejie25@users.noreply.github.com>
2023-01-15 20:08:08 +0800

    Update dm-streamlit.py

--- dm-streamlit.py
+++ dm-streamlit.py
@@ -1,6 +1,3 @@
-# required download pip package
-# pdfkit, wkhtmltopdf
-
 ##
 # 1. host it at streamlit cloud
 # 2. show EDA
@@ -11,7 +8,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
@@ -20,40 +16,42 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 import sklearn.metrics as metrics
 
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve, precision_recall_curve
-import pickle
+from sklearn.metrics import mean_absolute_error, r2_score, accuracy_score, confusion_matrix
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve
 from geopy.geocoders import Nominatim
 from sklearn.ensemble import RandomForestClassifier
 from boruta import BorutaPy
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
-from apyori import apriori
-
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-import pdfkit
 
 import warnings
 warnings.filterwarnings("ignore")
 
 pd.set_option("display.max_columns", 500)
 
-st.title("Project Visualize")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+import warnings
+warnings.filterwarnings("ignore")
+
+pd.set_option("display.max_columns", 500)
+
+st.title("Project Visualization")
 markdown_text = '''
 Project Members:
-1. 1181103230 Loo Chen Zhi
-2. 1181103362 Chang See Jie
-3. 1181103501 Lim Wei Jie
+1. 1181103230 Loo Chen Zhi  01116789079
+2. 1181103362 Chang See Jie 0143490382
+3. 1181103501 Lim Wei Jie   0125681547
 '''
 st.markdown(markdown_text)
 st.markdown("---")
 
-st.subheader(f"How we handling the missing values?")
+st.subheader(f"How we handle with the missing values?")
 st.markdown("- Fill with 'unknown'")
 st.markdown("- Fill with 'median'")
 st.markdown("- Fill with 'mode'")
@@ -117,17 +115,16 @@ st.write(f"After the pre-processing steps done, the final dataset consists of **
 
 st.markdown("---")
 st.subheader("Exploratory Data Analysis")
+
 st.write("**Insight 1: The majority of customers are Malay, with a significant number of Indian and Chinese customers as well.**")
 st.bar_chart(merged_df["Race"].value_counts())
+
 st.write("**Insight 2: The number of female customers are slighly more than the number of male customers.**")
 st.bar_chart(merged_df["Gender"].value_counts())
 
-
 st.write("**Insight 3: The majority of customers are between 30-40 years old.**")
 bins = [10,20,30,40,50,60]
-
 df['Age_Bin'] = pd.cut(df['Age_Range'], bins=bins)
-
 plt.figure(figsize=(12,8))
 df['Age_Bin'].value_counts().sort_index().plot(kind='bar')
 plt.title('Barchart for Binned Age')
@@ -138,13 +135,17 @@ plt.ylabel('Count')
 st.pyplot()
 
 st.markdown("---")
+
+
+
+################################################### Clustering ###################################################
+
 st.subheader("Question : Where are the customers located?")
-st.subheader("Technique: k-Means Clustering")
-st.write("We will perform k-means clustering to group the customers based on their geographic locations (latitude & longitude).")
+st.caption("To answer this question, K-means clustering is performed to group the customers based on their geographic location, which is the _latitude_ and _longitude_ columns.")
 
 km_df = merged_df[['latitude', 'longitude']]
 
-k = st.slider("Select the value of k", 1, 10, 5)
+k = st.slider("Select the value of k", 1, 10, 4)
 
 kmeans = KMeans(n_clusters=k)
 kmeans.fit(km_df[['latitude', 'longitude']])
@@ -159,7 +160,8 @@ plt.ylabel('Longitude')
 plt.title('Clusters of Customers\' Locations')
 st.pyplot()
 
-st.write("Majority center point detection for exact locations")
+st.caption('‘X’ symbol at the centre of each cluster to represent the centre points.')
+st.write("**It can be said that the customers are majorly located in:**")
 
 geolocator = Nominatim(user_agent="geoapiExercises")
 
@@ -170,7 +172,16 @@ for i in range(len(cluster_centers)):
     
 st.markdown("---")
 
+
+
+################################################### Classification ###################################################
+
 st.subheader("Question: Will a customer purchase drinks in the laundry shop?")
+st.caption('In this question, the _Drinks_ variable will be predicted to know whether or not the customers will buy drinks, where yes=1 and no=0.')
+st.caption('Logistic Regression is used to predict the target variable, _Drinks_.')
+st.caption('Boruta is used to performing feature selection to identify the most important features from the dataset, the top 10 features are selected')
+st.caption('The _Drinks_ variable is imbalanced, so oversampling has to be applied by using SMOTE.')
+st.caption('Accuracy, Confusion Matrix, Area Under the Curve (AUC) and Precision-Recall will be used to evaluate the performance.')
 
 clf_df = merged_df.copy()
 # st.dataframe(clf_df.head())
@@ -191,6 +202,7 @@ clf_X = clf_df.drop(['Drinks', 'buyDrinks'], axis=1)
 clf_y = clf_df['Drinks']
 clf_colnames = clf_X.columns
 
+##### boruta #####
 rf = RandomForestClassifier(n_jobs=-1, class_weight='balanced_subsample', max_depth=5)
 feat_selector = BorutaPy(rf, n_estimators='auto', random_state=1)
 
@@ -213,50 +225,44 @@ clf_y = clf_df['Drinks']
 
 clf_X_train, clf_X_test, clf_y_train, clf_y_test = train_test_split(clf_X, clf_y, test_size=0.2, random_state=42)
 
+##### SMOTE #####
 sm = SMOTE()
 X_train_smote, y_train_smote = sm.fit_resample(clf_X_train, clf_y_train)
 
-st.write("Original training set shape: ", clf_X_train.shape)
-st.write("Resampled training set shape: ", X_train_smote.shape)
-st.write("Original test set shape: ", clf_X_test.shape)
-st.write("Original target variable shape: ", clf_y_train.shape)
-st.write("Resampled target variable shape: ", y_train_smote.shape)
+# st.write("Original training set shape: ", clf_X_train.shape)
+# st.write("Resampled training set shape: ", X_train_smote.shape)
+# st.write("Original test set shape: ", clf_X_test.shape)
+# st.write("Original target variable shape: ", clf_y_train.shape)
+# st.write("Resampled target variable shape: ", y_train_smote.shape)
 
-c = st.slider('Regularization parameter C', 0.01, 10.0)
-
-logreg_clf = LogisticRegression(C=c)
-logreg_clf.fit(clf_X_train, clf_y_train)
-clf_y_pred = logreg_clf.predict(clf_X_test)
+##### Logistic Regression #####
+st.subheader("Logistic Regression")
 
 def evaluate_model(clf, X_train, y_train, colour, label):
-    st.write("Accuracy: {:.3f}".format(accuracy_score(clf_y_test, clf_y_pred)))
-    st.write("Accuracy on training set: {:.3f}".format(logreg_clf.score(X_train, y_train)))
-    st.write("Accuracy on test set: {:.3f}".format(logreg_clf.score(clf_X_test, clf_y_test)))
+    acc = round(accuracy_score(clf_y_test, clf_y_pred), 3)
+    acc_train = round(logreg_clf.score(X_train, y_train), 3)
+    acc_test = round(logreg_clf.score(clf_X_test, clf_y_test), 3)
 
     # confusion matrix
     cm = confusion_matrix(clf_y_test, clf_y_pred)
-    st.write('**********************')
     st.write('Majority TN =', cm[0][0])
     st.write('Majority FP =', cm[0][1])
     st.write('Majority FN =', cm[1][0])
     st.write('Majority TP =', cm[1][1])
-    st.write('**********************')
+
+    # display coefficients
+    coefficients = clf.coef_
+    st.write("Coefficients: ", coefficients)
 
     # calculate AUC
     prob = clf.predict_proba(clf_X_test)
     prob = prob[:, 1]
     auc = roc_auc_score(clf_y_test, prob)
-    st.write('AUC: %.2f' % auc)
 
     fpr, tpr, thresholds = roc_curve(clf_y_test, prob) # roc curve
     prec, rec, threshold = precision_recall_curve(clf_y_test, prob) # precision-recall curve
 
-    st.write("Precision-Recall: {:.2f}".format(metrics.auc(rec, prec)))
-
-    # class_names = ['0', '1']
-    # disp = plot_confusion_matrix(clf, clf_X_test, clf_y_test, display_labels=class_names, cmap=plt.cm.Blues)
-    # disp.ax_.set_title("Confusion Matrix")
-    # st.pyplot()
+    pr = round(metrics.auc(rec, prec), 2)
 
     # plot ROC Curve 
     plt.plot(fpr, tpr, color=colour, label=label) 
@@ -276,47 +282,76 @@ def evaluate_model(clf, X_train, y_train, colour, label):
     plt.legend()
     st.pyplot()
 
-    return fpr, tpr, prec, rec
+    return acc, acc_train, acc_test, cm, auc, pr
+
+# train the model
+logreg_clf = LogisticRegression()
+logreg_clf.fit(clf_X_train, clf_y_train)
+clf_y_pred = logreg_clf.predict(clf_X_test)
+
+st.write("**WITHOUT SMOTE**")
+
+acc, acc_train, acc_test, cm, auc, pr = evaluate_model(logreg_clf, clf_X_train, clf_y_train, 'orange', 'LogReg')
+
+# # Save the model
+# if st.button('Save Model'):
+#     pickle.dump(logreg_clf, open('logreg_clf.sav', 'wb'))
+#     st.success('Model saved!')
 
 
-def logreg_model():
-    # Create a slider for the regularization parameter C
+# train the model using SMOTE
+logreg_sm_clf = LogisticRegression()
+logreg_sm_clf.fit(X_train_smote, y_train_smote)
+clf_y_pred = logreg_sm_clf.predict(clf_X_test)
 
-    # train the model
-    logreg_clf = LogisticRegression(C=c)
-    logreg_clf.fit(clf_X_train, clf_y_train)
+st.write("**WITH SMOTE**")
 
-    clf_y_pred = logreg_clf.predict(clf_X_test)
+sm_acc, sm_acc_train, sm_acc_test, sm_cm, sm_auc, sm_pr = evaluate_model(logreg_sm_clf, X_train_smote, y_train_smote, 'blue', 'LogReg with SMOTE')
 
-    # model accuracy
-    st.write('--------------WITHOUT SMOTE--------------')
-    logreg_fpr, logreg_tpr, logreg_prec, logreg_rec = evaluate_model(logreg_clf, clf_X_train, clf_y_train, 'orange', 'LogReg')
+table = {'METRICS': ['Accuracy','Accuracy on training set', 'Accuracy on test set', 'AUC', 'Precision'],
+        'WITHOUT SMOTE': [acc, acc_train, acc_test,  auc, pr],
+        'WITH SMOTE': [sm_acc, sm_acc_train, sm_acc_test, sm_auc, sm_pr]}
 
-    # Save the model
-    if st.button('Save Model'):
-        pickle.dump(logreg_clf, open('logreg_clf.sav', 'wb'))
-        st.success('Model saved!')
+logreg_result = pd.DataFrame(table)
+logreg_result.set_index(logreg_result.columns[0], inplace=True)
+st.write(logreg_result)
 
-    # Display coefficients
-    coefficients = logreg_clf.coef_
-    st.write("Coefficients: ", coefficients)
+# feature sliders
+st.write('')
+st.write("**Adjust the features values to predict whether the customer will buy drinks or not:**")
 
-    # train the model using SMOTE
-    logreg_clf = LogisticRegression(C=c)
-    logreg_clf.fit(X_train_smote, y_train_smote)
-    clf_y_pred = logreg_clf.predict(clf_X_test)
+humidity_input = st.slider('humidity', 59.2, 93.1)
+winddir_input = st.slider('winddir', 5.1, 357.6)
+TimeSpent_minutes_input = st.slider('TimeSpent_minutes', 11.0, 60.0)
+Age_Range_input = st.slider('Age_Range', 18.0, 60.0)
+cloudcover_input = st.slider('cloudcover', 86.9, 96.5)
+visibility_input = st.slider('visibility', 1.2, 10.2)
+windspeed_input = st.slider('windspeed', 8.0, 25.9)
+temp_input = st.slider('temp', 25.1, 29.9)
+feelslikemin_input = st.slider('feelslikemin', 23.0, 29.2)
+feelslikemax_input = st.slider('feelslikemax', 33.0, 42.1)
 
-    # model accuracy
-    st.write('--------------WITH SMOTE--------------')
-    logreg_sm_fpr, logreg_sm_tpr, logreg_sm_prec, logreg_sm_rec = evaluate_model(logreg_clf, X_train_smote, y_train_smote, 'blue', 'LogReg with SMOTE')
+input = [humidity_input, winddir_input, TimeSpent_minutes_input, Age_Range_input, cloudcover_input, 
+        visibility_input, windspeed_input, temp_input, feelslikemin_input, feelslikemax_input]
 
+input_pred = logreg_clf.predict([input])
+input_sm_pred = logreg_sm_clf.predict([input])
 
-if __name__ == '__main__':
-    logreg_model()
+st.write("**Prediction results:**")
+st.write('Linear Regression without SMOTE dataset', input_pred[0])
+st.write('Linear Regression with SMOTE dataset', input_sm_pred[0])
 
 st.markdown("---")
 
+
+
+################################################### Regression ###################################################
+
 st.subheader("Question: What is the relationship between the weather conditions and the number of customers at the laundry shop?")
+st.caption('For this question, Linear Regression and Support Vector Regression are used to predict the number of customers based on the weather conditions using the external weather data.') 
+st.caption('Recursive Feature Elimination with Cross-Validation (RFECV) is used as a feature selection technique to select the most relevant features for the predictive models. The top 5 features are used for the Linear Regression and Support Vector Regression to do prediction.')
+st.caption('Hyperparameter tuning using GridSearch is done on both models to be compared with models without tuning.')
+st.caption('The Mean Absolute Error (MAE) and R-Squared (R2) are used to evaluate the models.')
 
 weather_cus = merged_df.drop(['Time', 'Race', 'Gender', 'Body_Size', 'Age_Range', 'With_Kids',
        'Kids_Category', 'Basket_Size', 'Basket_colour', 'Attire',
@@ -345,18 +380,18 @@ X = weather_cus.drop(['count'], axis=1)
 y = weather_cus['count']
 colnames = X.columns
 
-rf = RandomForestClassifier()
-rfe = RFECV(rf, cv=5)
+# rf = RandomForestClassifier()
+# rfe = RFECV(rf, cv=5)
 
-param_grid = {"estimator__n_estimators": [10, 50, 100],
-                "estimator__max_depth": [1, 5, 10],
-                "estimator__min_samples_leaf": [1, 2, 4]}
+# param_grid = {"estimator__n_estimators": [10, 50, 100],
+#                 "estimator__max_depth": [1, 5, 10],
+#                 "estimator__min_samples_leaf": [1, 2, 4]}
 
-grid_search = GridSearchCV(rfe, param_grid, cv=5)
-grid_search.fit(X, y)
+# grid_search = GridSearchCV(rfe, param_grid, cv=5)
+# grid_search.fit(X, y)
 
-st.write("Best parameters: {}".format(grid_search.best_params_))
-st.write("Best score: {:.2f}".format(grid_search.best_score_))
+# st.write("Best parameters: {}".format(grid_search.best_params_))
+# st.write("Best score: {:.2f}".format(grid_search.best_score_))
 
 ht_rf = RandomForestClassifier(n_estimators=10, max_depth=1, min_samples_leaf=1)
 rf.fit(X, y)
@@ -376,31 +411,56 @@ rfe_score = rfe_score.sort_values("Score", ascending = False)
 
 rfe_top_5 = rfe_score.Features[:5]
 
-# st.write('---------Top 10----------')
-# st.dataframe(rfe_score.head(10))
-
-# st.write('---------Bottom 10----------')
-# st.dataframe(rfe_score.tail(10))
-
 reg_X = weather_cus[rfe_top_5]
 reg_y = weather_cus['count']
 
-# split the data into training and test sets
 reg_X_train, reg_X_test, reg_y_train, reg_y_test = train_test_split(reg_X, reg_y, test_size=0.3, random_state=50)
 
-# Create the linear regression model
+st.write("**Linear Regression**")
+
 linreg = LinearRegression()
+linreg.fit(reg_X_train, reg_y_train)
+reg_y_pred = linreg.predict(reg_X_test)
 
-# Define the hyperparameters and their possible values
-parameters = {'fit_intercept':[True,False]}
+# calculate the mean absolute error and r-squared of the predictions
+mae_lin = mean_absolute_error(reg_y_test, reg_y_pred)
+r2_lin = r2_score(reg_y_test, reg_y_pred)
 
-# Perform grid search with cross-validation
-grid_search = GridSearchCV(linreg, parameters, cv=5)
+# hyperparameter tuning
+linreg = LinearRegression()
+parameters = {'fit_intercept':[True,False]} # define the hyperparameters and their possible values
+grid_search = GridSearchCV(linreg, parameters, cv=5) # perform grid search with cross-validation
 grid_search.fit(reg_X_train, reg_y_train)
+linreg = LinearRegression(**grid_search.best_params_) # train the final model on the entire training set
+linreg.fit(reg_X_train, reg_y_train)
+reg_y_pred = linreg.predict(reg_X_test) # evaluate the final model on the test set
 
-# Print the best parameters and score
-st.write("Best parameters: ", grid_search.best_params_)
-st.write("Best score: ", grid_search.best_score_)
+# calculate the mean absolute error and r-squared of the predictions
+mae_lin_ht = mean_absolute_error(reg_y_test, reg_y_pred)
+r2_lin_ht = r2_score(reg_y_test, reg_y_pred)
+
+table = {'Metrics': ['Mean Absolute Error','R2 Score'],
+        'Before Hyperparameter tuning': [mae_lin, r2_lin],
+        'After Hyperparameter tuning': [mae_lin_ht, r2_lin_ht]}
+
+lr_result = pd.DataFrame(table)
+lr_result.set_index(lr_result.columns[0], inplace=True)
+st.write(lr_result)
+
+st.write("**Support Vector Regression**")
+
+# create the SVR model
+svr = SVR(kernel='linear', C=1.0, epsilon=0.1)
+
+# fit the model to the training data
+svr.fit(reg_X_train, reg_y_train)
+
+# use the model to make predictions on the test data
+reg_y_pred = svr.predict(reg_X_test)
+
+# calculate the mean absolute error and r-squared of the predictions
+mae_svr = mean_absolute_error(reg_y_test, reg_y_pred)
+r2_svr = r2_score(reg_y_test, reg_y_pred)
 
 svr = SVR()
 
@@ -411,37 +471,27 @@ param_grid = {"C": [0.1, 1, 10],
     
 svr_grid_search = GridSearchCV(svr, param_grid, cv=5)
 svr_grid_search.fit(reg_X_train, reg_y_train)
-st.write("Best parameters: {}".format(svr_grid_search.best_params_))
 
+# create the SVR model
+svr = SVR(**svr_grid_search.best_params_)
 
-# def generate_report():
-#     data = pd.read_csv("dataset.csv")
+# fit the model to the training data
+svr.fit(reg_X_train, reg_y_train)
 
-#     report = ""
-#     report += "## Data Summary\n"
-#     report += f"Shape of the data: {data.shape}\n"
-#     report += f"Columns in the data: {data.columns}\n"
-#     report += f"Data Types: \n{data.dtypes}\n"
-#     report += f"Missing values: \n{data.isna().sum()}\n"
-#     report += "\n## Data Distribution\n"
-#     report += "### Numerical Columns\n"
-#     report += f"{data.describe()}\n"
-#     report += "### Categorical Columns\n"
-#     report += f"{data.select_dtypes(include='object').describe()}\n"
-#     report += "\n## Correlation\n"
-#     report += f"{data.corr()}\n"
-#     report += "\n## Distribution of Target Column\n"
-#     report += sns.countplot(data["target_column"])
-#     report += "\n"
-#     return report
+# use the model to make predictions on the test data
+reg_y_pred = svr.predict(reg_X_test)
 
-# # Generate the EDA report
-# report = generate_report()
+# calculate the mean absolute error and r-squared of the predictions
+mae_svr_ht = mean_absolute_error(reg_y_test, reg_y_pred)
+r2_svr_ht = r2_score(reg_y_test, reg_y_pred)
 
-# # Write the report to the app
-# st.write(report)
+table = {'Metrics': ['Mean Absolute Error','R2 Score'],
+        'Before Hyperparameter tuning': [mae_svr, r2_svr],
+        'After Hyperparameter tuning': [mae_svr_ht, r2_svr_ht]}
 
-# # Create a download button
-# if st.button('Download Report'):
-#     pdfkit.from_string(report, 'report.pdf')
-#     st.download('report.pdf')
+svr_result = pd.DataFrame(table)
+svr_result.set_index(svr_result.columns[0], inplace=True)
+st.write(svr_result)
+
+st.write('')
+st.write('To answer the question, both regression models are poorly performing. Both models are not suitable for predicting the number of customers based on the weather conditions. Therefore, we can conclude that the relationship between weather and the number of customers is weak.')
