d6d467dd9ea1c9f5e0a3d0f91de0b56e1b87d038
WJ <74594391+wjlim-14@users.noreply.github.com>
2023-01-15 08:28:14 +0800

    Update dm-streamlit.py

--- dm-streamlit.py
+++ dm-streamlit.py
@@ -7,12 +7,10 @@
 # 3. provide a way for user to input data and run predictive model to return results.
 # 4. extra -> allow download of a PDF report on EDA.
 ##
-pip install seaborn
-
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
